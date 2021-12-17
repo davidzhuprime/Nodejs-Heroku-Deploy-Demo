9a59b2aac2c80f8d097807c23f2846c7ab462797
davidzhuprime <davidzhuprime@gmail.com>
2021-12-18 04:22:31 +0800

    clock 2.

--- js/index.js
+++ js/index.js
@@ -1,40 +1,42 @@
-// const express = require('express')
-// const app = express()
-// const port = process.env.PORT || 3000
-//
-// /*
-//     Incase you are using mongodb atlas database uncomment below line
-//     and replace "mongoAtlasUri" with your mongodb atlas uri.
-// */
-// // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
-//
-// app.get('/', (req, res) => {
-//     res.send('fuck off!')
-// })
-//
-// app.listen(port, () => {
-//     console.log(`App listening at http://localhost:${port}`)
-// })
+const display = document.getElementById('clock');
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
 
-let clock = () => {
-    let date = new Date();
-    let hrs = date.getHours();
-    let mins = date.getMinutes();
-    let secs = date.getSeconds();
-    let period = "AM";
-    if (hrs == 0) {
-        hrs = 12;
-    } else if (hrs >= 12) {
-        hrs = hrs - 12;
-        period = "PM";
+function updateTime() {
+    const date = new Date();
+
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+
+
+
+    display.innerText=`${hour} : ${minutes} : ${seconds}`
+}
+
+function formatTime(time) {
+    if ( time < 10 ) {
+        return '0' + time;
     }
-    hrs = hrs < 10 ? "0" + hrs : hrs;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
+    return time;
+}
 
-    let time = `${hrs}:${mins}:${secs}:${period}`;
-    document.getElementById("clock").innerText = time;
-    setTimeout(clock, 1000);
-};
+function setAlarmTime(value) {
+    alarmTime = value;
+}
 
-clock();
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert('Alarm set');
+        }
+    }
+}
+setInterval(updateTime, 1000);
